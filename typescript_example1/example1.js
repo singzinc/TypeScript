3e96c054c44bdba897f26470f52991bff338d35c
singzinc <sing.zinc.hk@gmail.com>
2017-10-02 22:56:14 +0800

    build up the first example

--- typescript_example1/example1.js
+++ typescript_example1/example1.js
@@ -1,7 +1,5 @@
-function greeter (person: String){
+function greeter(person) {
     return "Hello, " + person;
-
 }
-
 var user = "test user 1";
 console.log("this is test : " + greeter(user));
---
+++ typescript_example1/readme.md
@@ -0,0 +1,8 @@
+run the command in termainal 
+tsc example1.ts
+
+
+The new file will be create (compile code)
+
+run the code 
+node example1.js
